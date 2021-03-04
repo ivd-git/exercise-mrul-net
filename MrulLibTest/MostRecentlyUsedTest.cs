b7067bce7f96b5e0ad6dc300a495164559e71097
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2021-03-04 16:40:38 +0100

    Empty project

---
+++ MrulLibTest/MostRecentlyUsedTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MrulLibTest
+{
+    [TestClass]
+    public class MostRecentlyUsedTest
+    {
+        [TestMethod]
+        public void TestSomething()
+        {
+        }
+    }
+}
